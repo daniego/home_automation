d224a57b0719dbbbc00a0918ce9e0b493d0587be
Daniego <daniel.floris@gmail.com>
2018-11-25 08:30:36 +0000

    major initial commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,23 @@
 
 export PATH=$PATH:/marytts
 
-cd marytts && marytts server &
+cd /marytts && marytts server &
 
-cd /home_automation/kalliope && kalliope start
+echo "Starting marytts... Wait"
+while true
+do
+  STATUS=$(curl -s -o /dev/null -w '%{http_code}' 127.0.0.1:59125/version)
+  if [ $STATUS -eq 200 ]; then
+    echo "Marytts Server READY!"
+    break
+  else
+    echo "."
+  fi
+  sleep 0.5
+done
+
+# cd /home_automation/kalliope && kalliope start  --debug
+# 2>&1
+# > /var/log/kalliope.log
+
+tail -f /var/log/*.log
